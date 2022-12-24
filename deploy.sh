75da426182c6a0a2c44df551f8f6dc92effc5a71
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-12-24 10:40:59 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -62,13 +62,13 @@ emerge net-misc/dhcpcd
 emerge net-wireless/wpa_supplicant
 emerge sys-boot/grub
 emerge app-admin/sudo
+emerge app-misc/neofetch
 
-ls /usr/share/zoneinfo
+
 echo "Australia/Brisbane" > /etc/timezone
 emerge --config sys-libs/timezone-data
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
-eselect locale list
 eselect locale set 4 # (US one just made)
 env-update && source /etc/profile
 
@@ -87,3 +87,14 @@ rc-service dhcpcd start
 grub-install $DISK # legacy
 # grub-install --target=x86_64-efi --efi-directory=/boot # UEFI
 grub-mkconfig -o /boot/grub/grub.cfg
+
+useradd -m -G users,wheel,audio,plugdev,video,sddm -s /bin/bash connor
+
+
+
+# Manual todo
+
+# visudo # uncomment wheel ALL=(ALL:ALL) ALL
+# nvim /etc/security/passwdqc.conf # enforce=none
+# passwd connor
+# nvim /etc/wpa_supplicant/wpa_supplicant.conf
