44fd03f151db43f73f74e6678dcb19289d8ea889
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-12-24 10:16:26 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,14 @@
 # stage3-amd64-desktop-openrc
 STAGE3URL=https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20221211T170150Z/stage3-amd64-desktop-openrc-20221211T170150Z.tar.xz
 
+# https://packages.gentoo.org/packages/sys-kernel/gentoo-kernel-bin
+KERNEL="sys-kernel/gentoo-kernel-bin:5.15.83"
+
 # fdisk -l
 DISK=/dev/sda
+
+# eselect profile list
+DESKTOP=9
 
 
 date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f4-10)" 
@@ -13,8 +19,43 @@ mkfs.ext4 "$DISK""3"
 mkswap "$DISK""2"
 swapon "$DISK""2"
 mkdir --parents /mnt/gentoo
-mount /dev/sda3 /mnt/gentoo
+mount "$DISK""3" /mnt/gentoo
 cd /mnt/gentoo
 wget $STAGE3URL
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 rm -rf stage3-*.tar.xz
+cd /mnt/gentoo/etc/portage
+rm make.conf
+wget https://raw.githubusercontent.com/Connor-McCartney/deploy-gentoo/main/make.conf
+mkdir --parents /mnt/gentoo/etc/portage/repos.conf
+cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
+cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+mount --bind /run /mnt/gentoo/run
+mount --make-slave /mnt/gentoo/run
+
+chroot /mnt/gentoo /bin/bash
+source /etc/profile
+mount "$DISK""1" /boot
+emerge-webrsync
+emerge --sync
+eselect profile set $DESKTOP
+emerge --verbose --update --deep --newuse @world
+emerge gui-apps/wl-clipboard
+emerge x11-misc/xclip
+emerge app-editors/neovim
+ls /usr/share/zoneinfo
+echo "Australia/Brisbane" > /etc/timezone
+emerge --config sys-libs/timezone-data
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+eselect locale list
+eselect locale set 4 # (US one just made)
+env-update && source /etc/profile
+emerge $KERNEL
+emerge sys-apps/pciutils
+emerge sys-kernel/linux-firmware
