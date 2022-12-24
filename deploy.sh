21d171cd46fdbde6d7ac1eef68ef5a0aada5b7df
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-12-24 19:42:07 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,15 @@
+STAGE3URL=https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20221211T170150Z/stage3-amd64-desktop-openrc-20221211T170150Z.tar.xz
+
+
+
+
+
+# https://www.gentoo.org/downloads/
+# stage3-amd64-desktop-openrc
+cd /mnt/gentoo
+wget $STAGE3URL
+
+
 
 # set datetime
 date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f4-10)" 
