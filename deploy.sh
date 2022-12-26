eaf0ff9b300689d272be30c8fe4d32b76632a636
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-12-27 09:16:52 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,11 @@ DISK=/dev/sda
 
 date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f4-10)" 
 # printf "g\nn\n1\n\n+256M\nt\n1\nn\n2\n\n+16G\nt\n2\n19\nn\n3\n\n\nw\n" | fdisk $DISK  # UEFU
+# mkfs.vfat -F 32 "$DISK""1"  # UEFI
+
 printf "o\nn\n\n\n\n+256M\nn\n\n\n\n+16G\nn\n\n\n\n\nt\n2\n82\na\n1\nw\n" | fdisk $DISK  # BIOS/LEGACY
-mkfs.vfat -F 32 "$DISK""1"
+mkfs.ext2 "$DISK""1"  # BIOS/LEGACY
+
 mkfs.ext4 "$DISK""3"
 mkswap "$DISK""2"
 swapon "$DISK""2"
