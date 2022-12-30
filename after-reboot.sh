626655ed6ca92ae57ab24a3599894470bd705255
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-12-31 08:13:48 +1000

    Update after-reboot.sh

--- after-reboot.sh
+++ after-reboot.sh
@@ -39,7 +39,6 @@ emerge sys-process/htop
 emerge net-libs/nodejs
 emerge gui-apps/wl-clipboard
 emerge x11-misc/xclip
-emerge media-fonts/iosevka
 emerge media-fonts/fonts-meta
 emerge net-im/discord-bin
 emerge app-admin/keepassxc
